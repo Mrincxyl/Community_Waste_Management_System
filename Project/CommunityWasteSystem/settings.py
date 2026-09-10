30690bdf4cda8dffbab808e5f928f51d80e21c8d
MrIncxyl <tsktechnical7@gmail.com>
2026-09-11 00:18:05 +0530

    Configure production deployment

--- Project/CommunityWasteSystem/settings.py
+++ Project/CommunityWasteSystem/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,10 +105,9 @@ WSGI_APPLICATION = "CommunityWasteSystem.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
